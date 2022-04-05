8aef07dd73d3cceaf46396cdda42564203dc04ab
Dulanys Cooray <dulanya@berkeley.edu>
2022-04-04 23:19:12 -0700

    GUI added - readings printed to serial + saved

--- COMBoard/COMBoard.ino
+++ COMBoard/COMBoard.ino
@@ -12,6 +12,7 @@ String success;
 int servo1_curr = 90;
 int servo2_curr = 0;
 int incomingS1 = 0;
+int read_count = 0;
 float incomingPT1 = 0;
 float incomingPT2 = 0;
 float incomingPT3 = 0;
@@ -32,23 +33,26 @@ uint8_t broadcastAddress[] = {0x24, 0x62, 0xAB, 0xD2, 0x85, 0xDC}; //change to n
 //Structure example to send data
 //Must match the receiver structure
 typedef struct struct_message {
-    float pt1;
-    float pt2;
-    float pt3;
-    float pt4;
-    float pt5;
-    float lc1;
-    float lc2;
-    float lc3;
-    float fm;
-    int S1;
-    int S2;
-    int S1S2;
-    boolean I;
+  float pt1;
+  float pt2;
+  float pt3;
+  float pt4;
+  float pt5;
+  float lc1;
+  float lc2;
+  float lc3;
+  float fm;
+  int S1;
+  int S2;
+  int S1S2;
+  boolean I;
 } struct_message;
 
 // Create a struct_message called Readings to recieve sensor readings remotely
 struct_message incomingReadings;
+
+// An array of all sets of readings that have been received so far
+struct_message Readings[];
 
 // Create a struct_message to send commands
 struct_message Commands;
@@ -57,17 +61,17 @@ struct_message Commands;
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   //Serial.print("\r\nLast Packet Send Status:\t");
   //Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-  if (status ==0){
+  if (status == 0) {
     success = "Delivery Success :)";
   }
-  else{
+  else {
     success = "Delivery Fail :(";
   }
 }
 
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
- // Serial.print("Bytes received: ");
+  // Serial.print("Bytes received: ");
   //Serial.println(len);
   incomingPT1 = incomingReadings.pt1;
   incomingPT2 = incomingReadings.pt2;
@@ -80,23 +84,25 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   incomingLC3 = incomingReadings.lc3;
   incomingS1 = incomingReadings.S1;
 
+  Readings[read_count] = incomingReadings;
+  read_count++;
 }
 void setup() {
   Commands.S1 = 90;
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(buttonpin1,INPUT);
-  pinMode(LEDpin,OUTPUT);
+  pinMode(buttonpin1, INPUT);
+  pinMode(LEDpin, OUTPUT);
 
   //set device as WiFi station
   WiFi.mode(WIFI_STA);
 
   //initialize ESP32
-   if (esp_now_init() != ESP_OK) {
+  if (esp_now_init() != ESP_OK) {
     //Serial.println("Error initializing ESP-NOW");
     return;
   }
-   // Once ESPNow is successfully Init, we will register for Send CB to
+  // Once ESPNow is successfully Init, we will register for Send CB to
   // get the status of Trasnmitted packet
   esp_now_register_send_cb(OnDataSent);
 
@@ -106,7 +112,7 @@ void setup() {
   peerInfo.encrypt = false;
 
   // Add peer
-  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
     //Serial.println("Failed to add peer");
     return;
   }
@@ -120,78 +126,109 @@ void loop() {
   pressed = digitalRead(buttonpin1); //push button to send servo signals
 
   // Serial.println("incomingS1, "+String(incomingS1)+", servo1_curr, "+String(servo1_curr));
-  if (incomingS1 == 90){
-  valveOpened = true;
-} else {
-  valveOpened = false;
-}
-if (valveOpened) {
-  digitalWrite(LEDpin,HIGH);
-} else {
-  digitalWrite(LEDpin,LOW);
-}
+  if (incomingS1 == 90) {
+    valveOpened = true;
+  } else {
+    valveOpened = false;
+  }
+  if (valveOpened) {
+    digitalWrite(LEDpin, HIGH);
+  } else {
+    digitalWrite(LEDpin, LOW);
+  }
 
-if (prevPressed && (millis() - pressTime > 5000)) {
-  prevPressed = false;
-  Commands.S1 = 90 - servo1_curr;
-  servo1_curr = 90 - servo1_curr;
-}
+  if (prevPressed && (millis() - pressTime > 5000)) {
+    prevPressed = false;
+    Commands.S1 = 90 - servo1_curr;
+    servo1_curr = 90 - servo1_curr;
+  }
 
   if (pressed && !prevPressed) {
     Commands.S1 = 90 - servo1_curr;
     servo1_curr = 90 - servo1_curr;
     pressTime = millis();
     //remove the following line with code that detects the status of the valve
-  //  valveOpened = !valveOpened;
-  // ADDED
-  prevPressed = pressed;
+    //  valveOpened = !valveOpened;
+    // ADDED
+    prevPressed = pressed;
   }
   //prevPressed = pressed;
 
   //servo2control
-//  currval2 = digitalRead(buttonpin2);
-//  if(prevval2==0 && currval2==1){
-//  Commands.S2 = 90 - servo2_curr;
-//  servo2_curr = 90 - servo2_curr;
-//  }
-//
-//  //servo1andservo2control
-//  currval3 = digitalRead(buttonpin3);
-//  if(prevval3==0 && currval3==1){
-//  Commands.S2 = 90 - servo2_curr;
-//  servo2_curr = 90 - servo2_curr;
-//  Commands.S1 = 90 - servo1_curr;
-//  servo1_curr = 90 - servo1_curr;
-//  }
+  //  currval2 = digitalRead(buttonpin2);
+  //  if(prevval2==0 && currval2==1){
+  //  Commands.S2 = 90 - servo2_curr;
+  //  servo2_curr = 90 - servo2_curr;
+  //  }
+  //
+  //  //servo1andservo2control
+  //  currval3 = digitalRead(buttonpin3);
+  //  if(prevval3==0 && currval3==1){
+  //  Commands.S2 = 90 - servo2_curr;
+  //  servo2_curr = 90 - servo2_curr;
+  //  Commands.S1 = 90 - servo1_curr;
+  //  servo1_curr = 90 - servo1_curr;
+  //  }
   //start igniter, leave on for a second
-//  Commands.I =  (digitalRead(buttonpin4)==1);
-//  if(Commands.I){
-//  delay(1000);}
-//  Commands.I = false;
-//  }
-//
+  //  Commands.I =  (digitalRead(buttonpin4)==1);
+  //  if(Commands.I){
+  //  delay(1000);}
+  //  Commands.I = false;
+  //  }
+  //
   //sends inputs for servos based on buttons
   esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &Commands, sizeof(Commands));
   if (result == ESP_OK) {
-   // Serial.println("Sent with success");
+    // Serial.println("Sent with success");
   }
   else {
     //Serial.println("Error sending the data");
   }
-//   prevval2 = currval2;
-//   prevval3 = currval3;
+  //   prevval2 = currval2;
+  //   prevval3 = currval3;
 
 
-///part to deal with MATLAB interfacing
+  // GUI Communication
+
+  //sends computer all readings received until that time when promtpted by the GUI
+  void sendReadings(){
+    if (Serial.availbale()){
+    String gui_instruction = Serial.read();
+    
+    if(gui_instruction == "Asking for readings"){      
+        for(int i = 0; i < read_count; i++){
+          printReadings(Readings[i], "PT", 4);
+          printReadings(Readings[i], "FM", 1);
+          printReadings(Readings[i], "LC", 3);
+          Serial.println();
+        }
+      }
+    }
+  }
+
+  // Given a set of readings outputs each *type* of reading (PT, FM, or LC) 
+  // to serial to communicate with processing GUI
+  void printReadings(struct_message curr_reading, String type, int num_readings){
+    String val;
+    for (int i = 0; i < num_readings; i++){
+      val = "incoming" + type + i;
+      Serial.print(curr_reading.val);
+      Serial.print(",");
+    }  
+  }
+
+  
+
+  ///part to deal with MATLAB interfacing
 
 
- Serial.print(millis());
-   Serial.print(" ");
-   // Print the flow rate for this second in litres / minute
-   // Serial.print("Flow rate: ");
-   //Serial.print(incomingFM);  // Print the integer part of the variable
-   //Serial.print("L/min");
-   //Serial.print(" ");       // Print tab space
+  Serial.print(millis());
+  Serial.print(" ");
+  // Print the flow rate for this second in litres / minute
+  // Serial.print("Flow rate: ");
+  //Serial.print(incomingFM);  // Print the integer part of the variable
+  //Serial.print("L/min");
+  //Serial.print(" ");       // Print tab space
 
 
 
@@ -199,32 +236,32 @@ if (prevPressed && (millis() - pressTime > 5000)) {
   //PT TEST
 
 
-   //Serial.print("Output Pressures: ");
-   Serial.print(incomingPT1);
-   Serial.print(" ");
-   Serial.print(incomingPT2);
-   Serial.print(" ");
-   Serial.print(incomingPT3);
-   Serial.print(" ");
-   Serial.print(incomingPT4);
-   Serial.print(" ");
-   Serial.print(incomingPT5);
-   Serial.print(" ");
-   //Serial.println("psi / ");
+  //Serial.print("Output Pressures: ");
+  Serial.print(incomingPT1);
+  Serial.print(" ");
+  Serial.print(incomingPT2);
+  Serial.print(" ");
+  Serial.print(incomingPT3);
+  Serial.print(" ");
+  Serial.print(incomingPT4);
+  Serial.print(" ");
+  Serial.print(incomingPT5);
+  Serial.print(" ");
+  //Serial.println("psi / ");
 
- //  Serial.print(" ");       // Print tab space
+  //  Serial.print(" ");       // Print tab space
 
- // FM Test
+  // FM Test
 
-    Serial.println(incomingFM);
+  Serial.println(incomingFM);
 
-    // Print the cumulative total of litres flowed since starting
-   //Serial.print("Output Liquid Quantity: ");
-    //Serial.print(totalMilliLitres);
-    //Serial.print("mL / ");
-    //Serial.print(totalMilliLitres / 1000);
-    //Serial.print("L");
-    //Serial.print("\t");       // Print tab space
+  // Print the cumulative total of litres flowed since starting
+  //Serial.print("Output Liquid Quantity: ");
+  //Serial.print(totalMilliLitres);
+  //Serial.print("mL / ");
+  //Serial.print(totalMilliLitres / 1000);
+  //Serial.print("L");
+  //Serial.print("\t");       // Print tab space
 
   //LC TEST
 
---
+++ COMBoard/COMBoard_GUI/COMBoard_GUI.pde
@@ -0,0 +1,115 @@
+import controlP5.*; 
+import processing.serial.*;
+import java.lang.*;
+
+Serial port;
+Table readings = new Table();
+
+
+ControlP5 cp5; 
+Textfield servo1, servo2;
+PFont font;
+String readingsFile;
+int readingCount = 0;
+
+void setup(){ 
+  size(300, 450); 
+  printArray(Serial.list());  
+  port = new Serial(this, "COM3", 9600);  //needs to change!! select right port
+  cp5 = new ControlP5(this);
+  font = createFont("calibri light bold", 20);
+  
+  readings.addColumn("PT1");
+  readings.addColumn("PT2");
+  readings.addColumn("PT3");
+  readings.addColumn("PT4");
+  readings.addColumn("FM");
+  readings.addColumn("LC1");
+  readings.addColumn("LC2");
+  readings.addColumn("LC3");
+  saveTable(readings, ###use absolute filepath"/readings.csv");
+  
+  //writes and saves data in .csv
+  cp5.addButton("store_readings")     
+    .setPosition(0, 50)  
+    .setSize(300, 35)      
+    .setFont(font)
+  ;     
+  
+  ////load cell data
+  //cp5.addButton("load_cell_data")     
+  //  .setPosition(0, 150)  
+  //  .setSize(300, 35)      
+  //  .setFont(font)
+  //;
+  
+  //servo 1 input
+  servo1 = cp5.addTextfield("servo1")   
+    .setPosition(0, 250) 
+    .setSize(300, 35)      
+    .setFont(font)
+  ;
+  
+  //servo 2 input
+  servo2 = cp5.addTextfield("servo2")     
+    .setPosition(0, 355)  
+    .setSize(300, 35)      
+    .setFont(font)
+  ;
+}
+
+void draw(){  //same as loop in arduino
+  background(0, 0 , 0); 
+  fill(0, 255, 0);             
+  textFont(font);  
+}
+
+//writes readings to a table
+void getReadings(Serial port){
+  String val = port.readStringUntil('\n'); //The newline separator separates each Arduino loop. We will parse the data by each newline separator. 
+  if (val!= null) { //We have a reading! Record it.
+    readingCount++; 
+    val = trim(val); //gets rid of any whitespace or Unicode nonbreakable space
+    val = val.replaceAll("[^\\d,]", "");
+    println(val); //Optional, useful for debugging. If you see this, you know data is being sent. Delete if  you like. 
+    float sensorVals[] = float(split(val, ',')); //parses the packet from Arduino and places the valeus into the sensorVals array. I am assuming floats. Change the data type to match the datatype coming from Arduino. 
+    
+   
+    TableRow newRow = readings.addRow(); //add a row for this new reading
+    newRow.setFloat("PT1", sensorVals[0]);
+    newRow.setFloat("PT2", sensorVals[1]);
+    newRow.setFloat("PT3", sensorVals[3]);
+    newRow.setFloat("PT4", sensorVals[4]);
+    newRow.setFloat("FM", sensorVals[5]);
+    newRow.setFloat("LC1", sensorVals[6]);
+    newRow.setFloat("LC2", sensorVals[7]);
+    newRow.setFloat("LC3", sensorVals[8]);
+  }
+}
+
+//writes + saves readings table in a .csv
+void store_readings(){
+   getReadings(port);
+   saveTable(readings, ######use absolute path here"/readings.csv");
+}
+
+//doesn't do anything rn
+void loading_pressure_data(){
+  port.write('p');
+}
+
+//doesn't do anything rn
+void load_cell_data(){
+  port.write('l');
+}
+
+void servo1() {
+  int servo1_pos = Integer.parseInt(servo1.getText()); 
+  port.write(1 + servo1_pos * 10); 
+  //port.write('b');
+}
+
+void servo2(){
+  int servo2_pos = Integer.parseInt(servo1.getText()); 
+  port.write(2 + servo2_pos * 10); 
+}
